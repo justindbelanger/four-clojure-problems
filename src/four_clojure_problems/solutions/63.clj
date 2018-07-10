c74800ffd625a10a0fa0b623040223f7ff485242
Justin Belanger <justindbelanger@outlook.com>
2018-07-09 20:05:14 -0700

    Solved #63

--- src/four_clojure_problems/solutions/63.clj
+++ src/four_clojure_problems/solutions/63.clj
@@ -1,11 +1,16 @@
-(fn [f s]
-  (reduce 
-   #(assoc 
-      %1 
-      (f %2) 
-      (if
-        (nil? (%1 (f %2)))
-        [%2]
-        (conj %1 %2)) ; if (f %2) is not a vector, create it. if it is, conj with it
-   {}
-   s))
+(ns four-clojure-problems.solutions.63)
+
+(def group (fn [f ss]
+             (reduce (fn [m s]
+                       (let [fs (f s)
+                             v (or (get m fs) [])]
+                         (assoc m fs (conj v s))))
+                     {}
+                     ss)))
+
+(def examples [{:input [#(> % 5) [1 3 6 8]]
+                :result {false [1 3], true [6 8]}}
+               {:input [#(apply / %) [[1 2] [2 4] [4 6] [3 6]]]
+                :result {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]}}
+               {:input [count [[1] [1 2] [3] [1 2 3] [2 3]]]
+                :result {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]}}])

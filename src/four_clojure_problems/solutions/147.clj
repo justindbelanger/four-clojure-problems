58c7b10fd54273199b53939169b507c4e3148022
Justin Belanger <justindbelanger@outlook.com>
2019-01-01 12:04:17 -0800

    Refactored solution to #147

--- src/four_clojure_problems/solutions/147.clj
+++ src/four_clojure_problems/solutions/147.clj
@@ -2,15 +2,12 @@
   (:require [clojure.test :refer [deftest is]]
             [four-clojure-problems.solutions.97 :as bug]))
 
-;; TODO can I use `iterate`?
-;; TODO eval to current, lazy-seq with lazy calculation for next
-;; TODO use `loop` and `recur` to avoid a stack overflow
 (defn pascal-trapezoid [row]
-  (let [next-row (vec (concat (list (first row))
-                              (bug/sum-as-pairs row)
-                              (list (last row))))]
-    (lazy-seq (cons row
-                    (pascal-trapezoid next-row)))))
+  (iterate (fn [current]
+             (vec (concat (list (first current))
+                          (bug/sum-as-pairs current)
+                          (list (last current)))))
+           row))
 
 (deftest pascal-trapezoid-tests
   (is (= (second (pascal-trapezoid [2 3 2]))
@@ -23,17 +20,17 @@
       (rest (take 101 (pascal-trapezoid [2 2])))))
 
 (def solution (fn pt [row]
-                (let [sum-as-pairs (fn sum-fn [[i & others]]
-                                     (let [j (first others)]
-                                       (if (and i j)
-                                         (let [summed (+' i j)
-                                               more (if others
-                                                      (lazy-seq (sum-fn others))
-                                                      '())]
-                                           (cons summed more))
-                                         '())))
-                      next-row (vec (concat (list (first row))
-                                            (sum-as-pairs row)
-                                            (list (last row))))]
-                  (lazy-seq (cons row
-                                  (pt next-row))))))
+              (let [sum-as-pairs (fn sum-fn [[i & others]]
+                                    (let [j (first others)]
+                                      (if (and i j)
+                                        (let [summed (+' i j)
+                                              more (if others
+                                                    (lazy-seq (sum-fn others))
+                                                    '())]
+                                          (cons summed more))
+                                        '())))
+                    next-row (vec (concat (list (first row))
+                                          (sum-as-pairs row)
+                                          (list (last row))))]
+                (lazy-seq (cons row
+                                (pt next-row))))))

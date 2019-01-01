19bc581755c4775b6190d77e910adc854670fac8
Justin Belanger <justindbelanger@outlook.com>
2019-01-01 11:41:18 -0800

    Test solution to #97 and support big numbers

--- src/four_clojure_problems/solutions/97.clj
+++ src/four_clojure_problems/solutions/97.clj
@@ -1,9 +1,10 @@
-(ns four-clojure-problems.solutions.97)
+(ns four-clojure-problems.solutions.97
+  (:require [clojure.test :refer [deftest is]]))
 
 (def sum-as-pairs (fn sum-fn [[i & others]]
                     (let [j (first others)]
                       (if (and i j)
-                        (let [summed (+ i j)
+                        (let [summed (+' i j)
                               more (if others
                                      (lazy-seq (sum-fn others))
                                      '())]
@@ -32,3 +33,15 @@
                     (concat [1]
                             (sum-fn (pt (- n 1)))
                             [1])))))
+
+(deftest pascal-triangle-tests
+  (is (= (pascal-triangle 1)
+         [1]))
+  (is (= (map pascal-triangle (range 1 6))
+         [     [1]
+          [1 1]
+          [1 2 1]
+          [1 3 3 1]
+          [1 4 6 4 1]]))
+  (is (= (pascal-triangle 11)
+         [1 10 45 120 210 252 210 120 45 10 1])))
